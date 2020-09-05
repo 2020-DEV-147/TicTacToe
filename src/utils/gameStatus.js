662e694cb2d5e7eea9d3855e9a1da91ed4a98d9c
React kata <kata4react@gmail.com>
2020-09-05 16:05:30 +0200

    Refactor the win logic

--- src/utils/gameStatus.js
+++ src/utils/gameStatus.js
@@ -1,27 +1,24 @@
 export const isWon = squares => {
-  if (squares[0] && squares[0] === squares[1] && squares[0] === squares[2]) {
-    return true
-  }
-  if (squares[3] && squares[3] === squares[4] && squares[3] === squares[5]) {
-    return true
-  }
-  if (squares[6] && squares[6] === squares[7] && squares[6] === squares[8]) {
-    return true
-  }
-  if (squares[0] && squares[0] === squares[3] && squares[0] === squares[6]) {
-    return true
-  }
-  if (squares[1] && squares[1] === squares[4] && squares[1] === squares[7]) {
-    return true
-  }
-  if (squares[2] && squares[2] === squares[5] && squares[2] === squares[8]) {
-    return true
-  }
-  if (squares[0] && squares[0] === squares[4] && squares[0] === squares[8]) {
-    return true
-  }
-  if (squares[2] && squares[2] === squares[4] && squares[2] === squares[6]) {
-    return true
+  const winCombinations = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6]
+  ]
+  for (let winCombination = 0; winCombination < winCombinations.length; winCombination++) {
+    const [position1, position2, position3] = winCombinations[winCombination];
+
+    const firstPositionFilled = squares[position1]
+    const matchWithSecondPosition = squares[position1] === squares[position2]
+    const matchWithThirdPosition = squares[position1] === squares[position3]
+
+    if (firstPositionFilled && matchWithSecondPosition && matchWithThirdPosition) {
+      return true
+    }
   }
   return false
 }
